6f1a4d62ab9803744785f6b1baadaf8d446fa329
Michael Carey <Michael@AGameEmpowerment.com>
2023-09-20 17:19:20 -0600

    Here is a code example with working text to speech synth.

---
+++ src/SpeechSynthExample/SpeechSynthExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Speech.Synthesis;
+
+// Initialize a new instance of the SpeechSynthesizer.  
+SpeechSynthesizer synth = new SpeechSynthesizer();
+
+// Configure the audio output.   
+synth.SetOutputToDefaultAudioDevice();
+
+//synth.SelectVoiceByHints(VoiceGender.Female);
+
+// Speak a string.  
+synth.Speak("Hello this is a code example demonstrating text to speech.");
+
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
